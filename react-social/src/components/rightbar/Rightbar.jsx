bcbf60f44cdd866b918d7e9f11bf547e27d8cec7
kATLEGO47 <katlegomasemene47@gmail.com>
2022-07-29 15:12:11 +0200

    Profile Page with Own it's RightBar front end

--- react-social/src/components/rightbar/Rightbar.jsx
+++ react-social/src/components/rightbar/Rightbar.jsx
@@ -3,9 +3,10 @@ import "./rightbar.css"
 import {Users} from "../../dummyData"
 import Online from "../online/Online"
 
-export default function Rightbar(){
-    return(
-        <div className="rightbar">
+export default function Rightbar({profile}){
+    const HomeRightbar = () =>{
+
+        return(
             <div className="rightbarWrapper">
                 <div className="birthdayContainer">
                     <img className="birthdayImg" src="/assets/gift.png" alt="" />
@@ -20,6 +21,63 @@ export default function Rightbar(){
                         <Online key={u.id} user={u}/>
                     ))}
                 </ul>            
+            </div>
+        )
+    }
+
+    const ProfileRightbar = () =>{
+        return(
+            <>
+            <h4 className="righbarTitle">User Information</h4>
+            <div className="rightbarInfo">
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">City:</span>
+                    <span className="rifgbarInfoValue">Braamfontein</span>
+                </div>
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">From:</span>
+                    <span className="rifgbarInfoValue">Cape town</span>
+                </div>
+                <div className="rightbarInfoItem">
+                    <span className="rightbarInfoKey">Relationship:</span>
+                    <span className="rifgbarInfoValue">Talkin Stage..</span>
+                </div>
+            </div>
+            <h4 className="righbarTitle">User Friends</h4>
+            <div className="rightbarFollowings">
+                <div className="rightbarFollowing">
+                    <img className="rightbarFollowingImg" src="/assets/person/1.jpg" alt="" />
+                    <span className="rightbarFollowingName">Marrium Jones</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img className="rightbarFollowingImg" src="/assets/person/2.jpg" alt="" />
+                    <span className="rightbarFollowingName">Marrium Jones</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img className="rightbarFollowingImg" src="/assets/person/3.jpg" alt="" />
+                    <span className="rightbarFollowingName">Marrium Jones</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img className="rightbarFollowingImg" src="/assets/person/4.jpg" alt="" />
+                    <span className="rightbarFollowingName">Marrium Jones</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img className="rightbarFollowingImg" src="/assets/person/5.jpg" alt="" />
+                    <span className="rightbarFollowingName">Marrium Jones</span>
+                </div>
+                <div className="rightbarFollowing">
+                    <img className="rightbarFollowingImg" src="/assets/person/6.jpg" alt="" />
+                    <span className="rightbarFollowingName">Marrium Jones</span>
+                </div>
+            </div>
+            </>
+        )
+    }
+
+    return(
+        <div className="rightbar">
+            <div className="rightbarWrapper">
+                <ProfileRightbar/>
             </div>
         </div>
     )
---
+++ react-social/src/pages/profile/Profile.jsx
@@ -0,0 +1,32 @@
+import "./profile.css"
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feedbar from "../../components/feedbar/Feedbar";
+import Rightbar from "../../components/rightbar/Rightbar";
+
+export default function Profile(){
+    return(
+        <>
+            <Topbar/>
+            <div className="profile">
+                <Sidebar/>
+                <div className="profileRight">
+                    <div className="profileRightTop">
+                        <div className="profileCover">
+                            <img className="profileCoverImg" src="/assets/post/3.jpg" alt="" />
+                            <img className="profileUserImg" src="/assets/person/10.jpeg" alt="" />
+                        </div>
+                        <div className="profileInfo">
+                            <h4 className="profileInfoName">Katlego Masemene</h4>
+                            <span className="profileInfoDesc">W'ssup Y'all</span>
+                        </div>
+                    </div>
+                    <div className="profileRightBottom">
+                        <Feedbar/>
+                        <Rightbar profile/>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
