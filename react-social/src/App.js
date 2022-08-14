01a2cab51b67c1c3cd0e931dfa2b51d8e29e129e
kATLEGO47 <katlegomasemene47@gmail.com>
2022-08-14 23:53:05 +0200

    Switch between pages incomplete

--- react-social/src/App.js
+++ react-social/src/App.js
@@ -3,9 +3,32 @@ import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
 
-function App() {
-  /*Navigate through by changing the Login tag eiter Home, Profile, Register*/ 
-  return <Login/>
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+function App() { 
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/login">
+          <Login/>
+        </Route>
+        <Route path="/register">
+          <Register/>
+        </Route>
+        <Route path="/profile/:username">
+          <Profile/>
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
+
